50bb669f936d921c799033d2423517f10b66eab2
Thach.Le <thachln@gmail.com>
2024-08-21 00:26:21 +0700

    Use CLIP: Version 1

--- install-libraries.cmd
+++ install-libraries.cmd
@@ -1,1 +1,2 @@
 pip install torch==2.3.1+cu121 torchaudio==2.3.1+cu121 torchvision==0.18.1+cu121 -f https://download.pytorch.org/whl/torch_stable.html
+pip install git+https://github.com/openai/CLIP.git
--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import torch
+import clip
+from PIL import Image
 
 print("Welcome the MyCLIP project.")
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 print('Use device:', device)
+
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+# Chuẩn bị hình ảnh
+image = preprocess(Image.open("data/muoi_nih.png")).unsqueeze(0).to(device)
+
+# Chuẩn bị văn bản
+text = clip.tokenize(["một con muỗi"]).to(device)
+
+with torch.no_grad():
+    image_features = model.encode_image(image)
+    text_features = model.encode_text(text)
+
+    # Tính toán điểm tương đồng (cosine similarity)
+    logits_per_image, logits_per_text = model(image, text)
+    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+print("Khả năng hình ảnh là 'một con muỗi'", probs)
+
